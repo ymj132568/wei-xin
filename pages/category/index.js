1657837515a5d237a0e932b199d6d06297832338
ymj132568 <69382176+ymj132568@users.noreply.github.com>
2020-09-06 17:22:27 +0800

    Update index.js

--- pages/category/index.js
+++ pages/category/index.js
@@ -10,7 +10,6 @@ Page({
     currentIndex: 0,
     scrollTop: 0
   },
-  // 接口的返回数据
   Cates: [],
 
   onLoad: function (options) {
@@ -37,57 +36,24 @@ Page({
     }
 
   },
-  // 获取分类数据
   async getCates() {
-    // request({
-    //   url: "/categories"
-    // })
-    //   .then(res => {
-    //     this.Cates = res.data.message;
-
-    //     // 把接口的数据存入到本地存储中
-    //     wx.setStorageSync("cates", { time: Date.now(), data: this.Cates });
-
-
-    //     // 构造左侧的大菜单数据
-    //     let leftMenuList = this.Cates.map(v => v.cat_name);
-    //     // 构造右侧的商品数据
-    //     let rightContent = this.Cates[0].children;
-    //     this.setData({
-    //       leftMenuList,
-    //       rightContent
-    //     })
-    //   })
-
-    // 1 使用es7的async await来发送请求
     const res = await request({ url: "/categories" });
-    // this.Cates = res.data.message;
     this.Cates = res;
-    // 把接口的数据存入到本地存储中
     wx.setStorageSync("cates", { time: Date.now(), data: this.Cates });
-    // 构造左侧的大菜单数据
     let leftMenuList = this.Cates.map(v => v.cat_name);
-    // 构造右侧的商品数据
     let rightContent = this.Cates[0].children;
     this.setData({
       leftMenuList,
       rightContent
     })
   },
-  // 左侧菜单的点击事件
   handleItemTap(e) {
-    /* 
-    1 获取被点击的标题身上的索引
-    2 给data中的currentIndex赋值就可以了
-    3 根据不同的索引来渲染右侧的商品内容
-     */
     const { index } = e.currentTarget.dataset;
 
     let rightContent = this.Cates[index].children;
     this.setData({
       currentIndex: index,
       rightContent,
-      // 重新设置 右侧内容的scroll-view标签的距离顶部的距离
       scrollTop: 0
     })
 
