37e2d140087d31e6236061e80c4b5a3f539e51d8
ymj132568 <69382176+ymj132568@users.noreply.github.com>
2020-09-06 17:30:02 +0800

    Update index.js

--- pages/collect/index.js
+++ pages/collect/index.js
@@ -36,12 +36,12 @@ Page({
       
   },
   handleTabsItemChange(e) {
-    // 1 获取被点击的标题索引
+  
     const { index } = e.detail;
-    // 2 修改源数组
+ 
     let { tabs } = this.data;
     tabs.forEach((v, i) => i === index ? v.isActive = true : v.isActive = false);
-    // 3 赋值到data中
+
     this.setData({
       tabs
     })
